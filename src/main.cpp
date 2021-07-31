d7096b8bc4988523e49e60d74e1ebf8050defb55
Jincheng Chen <chjinche@microsoft.com>
2021-07-31 16:57:52 +0000

    test and debug

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,33 @@
  * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
 #include <LightGBM/application.h>
+#include <LightGBM/c_api.h>
+
 
 #include <iostream>
 
 #include "network/linkers.h"
 
-int main(int argc, char** argv) {
+// int main(int argc, char** argv) {
+int main() {
+  // int * out_num_iters;
+  // *out_num_iters = 10;
+  // BoosterHandle* out;
+  // LGBM_BoosterCreateFromModelfile(
+  //   "/mnt/chjinche/projects/LightGBM/tests/data/trained_model",
+  //   out_num_iters, out,
+  //   "tmp_transform");
+
+  int argc = 2;
+  // char* argv[] = {"../../lightgbm", "config=/mnt/chjinche/projects/LightGBM/examples/multiclass_classification/train.conf"};
+  char* argv[] = {"../../lightgbm", "config=/mnt/chjinche/projects/LightGBM/tests/data/l2_train.conf"};
+  // char* argv[] = {"../../lightgbm", "config=/mnt/chjinche/projects/LightGBM/tests/cpp_tests/predict.conf"};
+  // char* argv[] = {"../../lightgbm", "config=/mnt/chjinche/projects/LightGBM/tests/data/l2_predict.conf"};
   bool success = false;
   try {
     LightGBM::Application app(argc, argv);
     app.Run();
-
-#ifdef USE_MPI
+ #ifdef USE_MPI
     LightGBM::Linkers::MpiFinalizeIfIsParallel();
 #endif
 
